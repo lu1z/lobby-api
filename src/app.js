4472021daaee99c407631d3b4f2ca244b6fb9d99
Luiz Rosa <luiz.alberto.laus.rosa.com>
2024-07-18 18:36:23 -0300

    basic functionality

---
+++ src/app.js
@@ -0,0 +1,109 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { MongoClient, ObjectId } from "mongodb";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.set('trust proxy', true);
+
+const connectionString = process.env.DATABASE_URL;
+const [, db] = connectionString.match(/mongodb.net\/(.*)$/);
+
+app.get('/ip', (req, res) => {
+    return res.status(200).json(req.ip);
+});
+
+app.get('/lobbies', async (req, res) => {
+    let client = null;
+    try {
+        client = new MongoClient(connectionString);
+        const database = client.db(db);
+        const collection = database.collection('lobbies');
+        let filter = {}
+        // if ('player' in req.body) filter = { player: req.body.player };
+        // if ('_id' in req.body) filter = { _id: new ObjectId(req.body._id) };
+        const result = await records.find(filter).toArray();
+        return res.status(200).json(result)
+    } finally {
+        if (client) await client.close();
+    }
+});
+
+app.post('/lobbies', async (req, res) => {
+    if (!('name' in req.body)) return res.status(400).json({ message: "Missing field 'name'!" });
+    if (!('port' in req.body)) return res.status(400).json({ message: "Missing field 'port'!" });
+    const name = req.body.name;
+    if (!name) return res.status(400).json({ message: "Field 'name' must not be empty!" });
+    const port = Number.parseInt(req.body.port);
+    if (!Number.isInteger(port)) return res.status(400).json({ message: "Field 'port' must be an integer!" });
+    if (port < 8100 || port > 65355) return res.status(400).json({ message: "Field 'port' must be inside (8100 .. 65355) integer interval!" });
+    let client = null;
+    try {
+        client = new MongoClient(connectionString);
+        const database = client.db(db);
+        const lobbies = database.collection('lobbies');
+        const isDuplicated = await lobbies.findOne({ name });
+        if (isDuplicated) return res.status(400).json({ message: `A lobby with name: '${name}' already exists, please chose pick another!` });
+        const address = req.ip;
+        const status = 'open';
+        const lobby = { name, address, port, status };
+        const { insertedId } = await lobbies.insertOne(lobby);
+        return res.status(201).json({ _id: insertedId, ...lobby });
+    } finally {
+        if (client) await client.close();
+    }
+});
+
+app.delete('/lobbies/:name', async (req, res) => {
+    let client = null;
+    try {
+        client = new MongoClient(connectionString);
+        const database = client.db(db);
+        const lobbies = database.collection('lobbies');
+        const { deletedCount } = await lobbies.deleteOne({ name: req.params.name });
+        if (!deletedCount) return res.sendStatus(404);
+        return res.sendStatus(204);
+    } finally {
+        if (client) await client.close();
+    }
+});
+
+app.delete('/lobbies', async (req, res) => {
+    let client = null;
+    try {
+        client = new MongoClient(connectionString);
+        const database = client.db(db);
+        const lobbies = database.collection('lobbies');
+        const { deletedCount } = await lobbies.deleteMany({});
+        if (!deletedCount) return res.sendStatus(404);
+        return res.sendStatus(204);
+    } finally {
+        if (client) await client.close();
+    }
+});
+
+app.patch('/lobbies/:name', async (req, res) => {
+    let client = null;
+    try {
+        client = new MongoClient(connectionString);
+        const database = client.db(db);
+        const lobbies = database.collection('lobbies');
+        const required = ["op", "path", "value"];
+        if (required.every((key) => key in req.body)) {
+            if (req.body.op === 'op' && req.body.path === '/status' && (req.body.value === 'open' || req.body.value === 'started')) {
+                const { matchedCount, modifiedCount } = await lobbies.updateOne({ name: req.params.name }, { $set: { status: req.body.value } });
+                if (!matchedCount) return res.sendStatus(404);
+                if (!modifiedCount) return res.sendStatus(204);
+                return res.status(400).json({ message: 'Incopetente!' });
+            }
+        }
+        return res.status(400).json({
+            message: 'Request body must be a variation of template: { "op": "replace", "path": "/status",  "value": "started|open" }!'
+        });
+    } finally {
+        if (client) await client.close();
+    }
+});
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import app from "./app.js";
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`server running in port ${port}`));
