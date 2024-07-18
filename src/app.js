bf5d58596c9dc59ae939928dbbf5583d95be4034
Luiz Rosa <luiz.alberto.laus.rosa.com>
2024-07-18 18:57:05 -0300

    fix get lobbies end point

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/lobbies', async (req, res) => {
         let filter = {}
         // if ('player' in req.body) filter = { player: req.body.player };
         // if ('_id' in req.body) filter = { _id: new ObjectId(req.body._id) };
-        const result = await records.find(filter).toArray();
+        const result = await collection.find(filter).toArray();
         return res.status(200).json(result)
     } finally {
         if (client) await client.close();
@@ -42,13 +42,13 @@ app.post('/lobbies', async (req, res) => {
     try {
         client = new MongoClient(connectionString);
         const database = client.db(db);
-        const lobbies = database.collection('lobbies');
-        const isDuplicated = await lobbies.findOne({ name });
+        const collection = database.collection('lobbies');
+        const isDuplicated = await collection.findOne({ name });
         if (isDuplicated) return res.status(400).json({ message: `A lobby with name: '${name}' already exists, please chose pick another!` });
         const address = req.ip;
         const status = 'open';
         const lobby = { name, address, port, status };
-        const { insertedId } = await lobbies.insertOne(lobby);
+        const { insertedId } = await collection.insertOne(lobby);
         return res.status(201).json({ _id: insertedId, ...lobby });
     } finally {
         if (client) await client.close();
@@ -60,8 +60,8 @@ app.delete('/lobbies/:name', async (req, res) => {
     try {
         client = new MongoClient(connectionString);
         const database = client.db(db);
-        const lobbies = database.collection('lobbies');
-        const { deletedCount } = await lobbies.deleteOne({ name: req.params.name });
+        const collection = database.collection('lobbies');
+        const { deletedCount } = await collection.deleteOne({ name: req.params.name });
         if (!deletedCount) return res.sendStatus(404);
         return res.sendStatus(204);
     } finally {
@@ -74,8 +74,8 @@ app.delete('/lobbies', async (req, res) => {
     try {
         client = new MongoClient(connectionString);
         const database = client.db(db);
-        const lobbies = database.collection('lobbies');
-        const { deletedCount } = await lobbies.deleteMany({});
+        const collection = database.collection('lobbies');
+        const { deletedCount } = await collection.deleteMany({});
         if (!deletedCount) return res.sendStatus(404);
         return res.sendStatus(204);
     } finally {
@@ -88,11 +88,11 @@ app.patch('/lobbies/:name', async (req, res) => {
     try {
         client = new MongoClient(connectionString);
         const database = client.db(db);
-        const lobbies = database.collection('lobbies');
+        const collection = database.collection('lobbies');
         const required = ["op", "path", "value"];
         if (required.every((key) => key in req.body)) {
             if (req.body.op === 'op' && req.body.path === '/status' && (req.body.value === 'open' || req.body.value === 'started')) {
-                const { matchedCount, modifiedCount } = await lobbies.updateOne({ name: req.params.name }, { $set: { status: req.body.value } });
+                const { matchedCount, modifiedCount } = await collection.updateOne({ name: req.params.name }, { $set: { status: req.body.value } });
                 if (!matchedCount) return res.sendStatus(404);
                 if (!modifiedCount) return res.sendStatus(204);
                 return res.status(400).json({ message: 'Incopetente!' });
