78117d60a9dcdd23471156a82bf0344e03c41413
Luiz Rosa <luiz.alberto.laus.rosa.com>
2024-07-20 17:40:04 -0300

    fix patch bug where condition was reversed

--- src/middlewares/bodyFieldValue.js
+++ src/middlewares/bodyFieldValue.js
@@ -4,7 +4,7 @@ import { incorrectValueField } from '../helpers/constants.js'
 export default function (pairs) {
     return (req, res, next) => {
         for (const [field, value] of pairs) {
-            if (req.body[field] === value) return res.status(400).json(incorrectValueField(field, value));
+            if (req.body[field] !== value) return res.status(400).json(incorrectValueField(field, value));
         }
         next();
     }
